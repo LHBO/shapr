05fd1121b0f6eef189fc5bb00d925b8a99a89e04
LHBO <lholsen@math.uio.no>
2024-01-16 12:05:51 +0000

    UiO fix install

--- R/Lars_explore_ideas_scripts/Run_linear_experiment.R
+++ R/Lars_explore_ideas_scripts/Run_linear_experiment.R
@@ -206,11 +206,11 @@ if (UiO) {
   # print("#HE")
   # devtools::clean_dll()
   # devtools::install_github(repo = "LHBO/shapr", ref = "Lars/paper3_ideas")
-  # # If they have the old version, then we install the one on GitHub
-  # if (any(as.list(args(shapr::explain)) == "x")) {
-  #   devtools::clean_dll()
-  #   devtools::install_github(repo = "LHBO/shapr", ref = "Lars/paper3_ideas")
-  # }
+  # If they have the old version, then we install the one on GitHub
+  if (any(as.list(args(shapr::explain)) == "x")) {
+    devtools::clean_dll()
+    devtools::install_github(repo = "LHBO/shapr", ref = "Lars/paper3_ideas")
+  }
 }
 # devtools::load_all(".")
 if (Sys.info()[[4]] == "nam-shub-02.uio.no") {
