28af857987757da6b4cd984f0c1a688430a00c57
LHBO <lholsen@math.uio.no>
2024-05-03 10:01:20 +0200

    Typo

--- R/Lars_explore_ideas_scripts/Run_linear_experiment.R
+++ R/Lars_explore_ideas_scripts/Run_linear_experiment.R
@@ -92,7 +92,6 @@
 #Rscript Run_linear_experiment.R FALSE FALSE TRUE FALSE 1:2 6 1000000 1000000 1000 1000 10 0.0,0.1,0.3 TRUE 1,1,1,1,1,1,1,1,1,1,1 NULL NULL (adroa)
 #Rscript Run_linear_experiment.R FALSE FALSE TRUE FALSE 1:2 6 1000000 1000000 1000 1000 10 0.6,0.9,0.99 TRUE 1,1,1,1,1,1,1,1,1,1,1 NULL NULL ()
 
-
 # Input From Command Line ----------------------------------------------------------------------------------------------
 args = commandArgs(trailingOnly = TRUE)
 # test if there is at least one argument: if not, return an error
@@ -352,7 +351,7 @@ sampling_methods = c("unique",
                      # "paired_coalitions_scaled",
                      # "paired_coalitions_avg",
                      # "paired_coalitions_norm",
-                     "single_mean_coalition_effect",
+                     "single_mean_coalition_effect"
                      # "single_median_coalition_effect",
                      # "single_mean_ranking_over_each_test_obs",
                      # "single_median_ranking_over_each_test_obs"
