50d150604bff8c6fd48562f82c34a4a8fd6f59c5
LHBO <lholsen@math.uio.no>
2024-08-28 17:00:39 +0200

    xgboost new strat

--- R/Lars_explore_ideas_scripts/Simulation_Extra_Strategies_XGBOOST.R
+++ R/Lars_explore_ideas_scripts/Simulation_Extra_Strategies_XGBOOST.R
@@ -56,13 +56,23 @@ sum_shapley_weights <- function(m){
 repetitions = 50
 rhos = c(0, 0.2, 0.5, 0.9)
 new_strategies = c("on_all_cond", "on_all_cond_paired")
+new_strategies = c("on_all_cond_paired")
 
 versions = c("largest_weights_random", "unique_paired")
 
-weight_version = "analytical"
+weight_versions = c("analytical", "non_analytical", "mean_L", "mean_ps")
+weight_versions = c("mean_L", "mean_ps")
 
 resave = TRUE
 
+dt_new_weights
+
+folder = "/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location"
+
+# Get the weights
+dt_new_weights_analytical = readRDS(file.path(folder, paste0("Analytical_prop_M_", 10, "_res.rds")))
+dt_new_weights_sequence = readRDS(file.path(folder, paste0("Sequence_length_M_10_combined.rds")))
+dt_new_weights_sequence = dcast(dt_new_weights_sequence[type == "mean"], M + N_S + Size ~ version, value.var = "Ps_tilde")
 
 
 # Iterate over the dependencies
@@ -106,93 +116,151 @@ for (rho in rhos) {
       # Iterate over the strategies
       strategy = new_strategies[1]
       for (strategy in new_strategies) {
-        strategy_name = paste(strategy, version, weight_version, sep = "_")
+        for (weight_version in weight_versions) {
+          strategy_name = paste(strategy, version, weight_version, sep = "_")
 
-        # Get the strategy specific save name
-        save_name = paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/Gompertz_Xgboost_M_10_n_train_1000_n_test_1000_rho_", rho, "_equi_TRUE_betas_2_10_0.25_-3_-1_1.5_-0.5_10_1.25_1.5_-2_estimated_repetition_", repetition ,"_",  strategy_name, ".rds")
+          # Get the strategy specific save name
+          save_name = paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/Gompertz_Xgboost_M_10_n_train_1000_n_test_1000_rho_", rho, "_equi_TRUE_betas_2_10_0.25_-3_-1_1.5_-0.5_10_1.25_1.5_-2_estimated_repetition_", repetition ,"_",  strategy_name, ".rds")
 
-        # List to store the results for each new strategy
-        save_list = list()
-        save_list[[strategy]] = list(repetition_1 = list())
+          # List to store the results for each new strategy
+          save_list = list()
+          save_list[[strategy_name]] = list(repetition_1 = list())
 
-        # Iterate over the different number of coalitions
-        n_comb_now_idx = 2
-        for (n_comb_now_idx in seq_along(n_combinations)) {
-          message(sprintf("version = %s \t weight_version = %s \t rho = %.1f \t repetition = %d \t strategy = %s \t n_comb = %d",
-                          version, weight_version, rho, repetition, strategy, n_combinations[n_comb_now_idx]))
+          # Iterate over the different number of coalitions
+          n_comb_now_idx = 20
+          for (n_comb_now_idx in seq_along(n_combinations)) {
+            message(sprintf("version = %s \t weight_version = %s \t rho = %.1f \t repetition = %d \t strategy = %s \t n_comb = %d",
+                            version, weight_version, rho, repetition, strategy, n_combinations[n_comb_now_idx]))
 
-          if (n_combinations[n_comb_now_idx] == 2) {
-            # Only empty and grand coalition so just copy
-            save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = file_relevant$n_combinations_2
-          } else {
-            #
-            file_now = file_relevant[[n_comb_now_idx]]
+            if (n_combinations[n_comb_now_idx] == 2) {
+              # Only empty and grand coalition so just copy
+              save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = file_relevant$n_combinations_2
+            } else {
+              #
+              file_now = file_relevant[[n_comb_now_idx]]
 
-            this_X = data.table::copy(file_now$only_save$X)
+              # # FJERN
+              # file_now = rrr$unique_paired$repetition_1$n_combinations_20
 
-            if (version == "largest_weights_random") { # Set weights to 1 as the coals are not sampled with replacement
-              this_X[-c(1,.N), shapley_weight := 1.0]
+              this_X = data.table::copy(file_now$only_save$X)
+
+              # # FJERN
+              # this_X[-c(1,.N), sum(shapley_weight)]
+              # shapley_reweighting(this_X, reweight = "on_all_cond")
+              # this_X[-c(1,.N), sum(shapley_weight)]
+              # this_X[-c(1,.N), shapley_weight := shapley_weight / sum(shapley_weight)]
+              # this_X[-c(1,.N), sum(shapley_weight)]
+              # this_X
+              #
+              #
+              # shapley_reweighting(this_X, reweight = "on_all_cond_paired")
+              # this_X[-c(1,.N), sum(shapley_weight)]
+              # this_X[-c(1,.N), shapley_weight := shapley_weight / sum(shapley_weight)]
+              # this_X[-c(1,.N), sum(shapley_weight)]
+              # this_X
+
+
+              if (version == "largest_weights_random") { # Set weights to 1 as the coals are not sampled with replacement
+                this_X[-c(1,.N), shapley_weight := 1.0]
+              }
+
+              # Re weight the shapely weights
+              shapley_reweighting(this_X, reweight = strategy)
+
+              if (weight_version == "analytical") {
+                dt_new_weights = copy(dt_new_weights_analytical)
+
+                # Find the weights of the combination closest to n_combinations
+                n_comb_use = dt_new_weights$n_combinations[which.min(abs(dt_new_weights$n_combinations - n_combinations[n_comb_now_idx]))]
+                dt_new_weights_now = dt_new_weights[n_combinations == n_comb_use]
+
+                # Update the weights with the provided weights for each coalition size
+                this_X[dt_new_weights_now, on = "n_features", shapley_weight := get("mean")]
+              } else if (weight_version == "mean_L") {
+
+                # Get the weights
+                dt_new_weights = copy(dt_new_weights_sequence)
+
+                # Find the weights of the combination closest to n_combinations
+                n_comb_use = dt_new_weights$N_S[which.min(abs(dt_new_weights$N_S - n_combinations[n_comb_now_idx]))]
+                dt_new_weights_now = dt_new_weights[N_S == n_comb_use]
+
+                dt_new_weights_now <- rbind(dt_new_weights_now, dt_new_weights_now[(.N - ifelse(n_row %% 2 == 1, 1, 0)):1])
+                dt_new_weights_now[, Size := seq(.N)]
+                setnames(dt_new_weights_now, "Size", "n_features")
+
+                # Update the weights with the provided weights for each coalition size
+                this_X[dt_new_weights_now, on = "n_features", shapley_weight := get("mean L")]
+
+              } else if (weight_version == "mean_ps") {
+
+                # Get the weights
+                dt_new_weights = copy(dt_new_weights_sequence)
+
+                # Find the weights of the combination closest to n_combinations
+                n_comb_use = dt_new_weights$N_S[which.min(abs(dt_new_weights$N_S - n_combinations[n_comb_now_idx]))]
+                dt_new_weights_now = dt_new_weights[N_S == n_comb_use]
+                dt_new_weights_now = dcast(dt_new_weights_now[type == "mean"], M + N_S + Size ~ version, value.var = "Ps_tilde")
+                dt_new_weights_now
+
+
+                dt_new_weights_now <- rbind(dt_new_weights_now, dt_new_weights_now[(.N - ifelse(n_row %% 2 == 1, 1, 0)):1])
+                dt_new_weights_now[, Size := seq(.N)]
+                setnames(dt_new_weights_now, "Size", "n_features")
+
+                # Update the weights with the provided weights for each coalition size
+                this_X[dt_new_weights_now, on = "n_features", shapley_weight := get("mean ps")]
+
+              } else {
+                stop("Unknown weight_version")
+              }
+
+              # Get the weight matrix
+              this_W <- shapr:::weight_matrix(X = this_X, normalize_W_weights = TRUE)
+
+              # Get the number of features
+              m = this_X[.N, n_features]
+
+              ## Get feature matrix ---------
+              S_sampled <- shapr:::feature_matrix_cpp(features = this_X[["features"]], m = m)
+
+              # Create the S matrix if we had used all combinations
+              S_all = shapr:::feature_matrix_cpp(
+                features = unlist(lapply(0:m, utils::combn, x = m, simplify = FALSE), recursive = FALSE), m = m
+              )
+              S_all_list = as.list(seq(nrow(S_all)))
+              names(S_all_list) = apply(S_all, 1, paste, collapse = "")
+
+              # Get a mapping from the indices of the current set of combinations/coalitions to the indices
+              # in the version where we use all 2^M combinations/coalitions.
+              current_combination_idx_in_all_combinations = as.numeric(S_all_list[apply(S_sampled, 1, paste, collapse = "")])
+
+              # Use the pre-computed v(S) data provided by the user
+              processed_vS_list = precomputed_vS
+
+              # Extract only the relevant rows (combinations) from the
+              processed_vS_list$dt_vS = processed_vS_list$dt_vS[current_combination_idx_in_all_combinations,]
+              processed_vS_list$dt_vS[, id_combination := .I]
+
+              # Compute the new Shapley values
+              shap_names <- true$internal$parameters$feature_names
+              kshap <- t(this_W %*% as.matrix(processed_vS_list$dt_vS[, -"id_combination"]))
+              dt_kshap <- data.table::as.data.table(kshap)
+              colnames(dt_kshap) <- c("none", shap_names)
+              dt_shapley = dt_kshap
+
+              # Store the results
+              save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = list(shapley_values = dt_shapley,
+                                                                                            MSEv = NULL,
+                                                                                            only_save = list(X = this_X,
+                                                                                                             W = this_W,
+                                                                                                             S = S_sampled))
             }
-
-            # Re weight the shapely weights
-            shapley_reweighting(this_X, reweight = strategy)
-
-            if (weight_version == "analytical") {
-              dt_new_weights = readRDS(paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/Analytical_prop_M_", 10, "_res.rds"))
-
-              # Find the weights of the combination closest to n_combinations
-              n_comb_use = dt_new_weights$n_combinations[which.min(abs(dt_new_weights$n_combinations - n_combinations[n_comb_now_idx]))]
-              dt_new_weights_now = dt_new_weights[n_combinations == n_comb_use]
-
-              # Update the weights with the provided weights for each coalition size
-              this_X[dt_new_weights_now, on = "n_features", shapley_weight := get("mean")]
-            }
-
-            # Get the weight matrix
-            this_W <- shapr:::weight_matrix(X = this_X, normalize_W_weights = TRUE)
-
-            # Get the number of features
-            m = this_X[.N, n_features]
-
-            ## Get feature matrix ---------
-            S_sampled <- shapr:::feature_matrix_cpp(features = this_X[["features"]], m = m)
-
-            # Create the S matrix if we had used all combinations
-            S_all = shapr:::feature_matrix_cpp(
-              features = unlist(lapply(0:m, utils::combn, x = m, simplify = FALSE), recursive = FALSE), m = m
-            )
-            S_all_list = as.list(seq(nrow(S_all)))
-            names(S_all_list) = apply(S_all, 1, paste, collapse = "")
-
-            # Get a mapping from the indices of the current set of combinations/coalitions to the indices
-            # in the version where we use all 2^M combinations/coalitions.
-            current_combination_idx_in_all_combinations = as.numeric(S_all_list[apply(S_sampled, 1, paste, collapse = "")])
-
-            # Use the pre-computed v(S) data provided by the user
-            processed_vS_list = precomputed_vS
-
-            # Extract only the relevant rows (combinations) from the
-            processed_vS_list$dt_vS = processed_vS_list$dt_vS[current_combination_idx_in_all_combinations,]
-            processed_vS_list$dt_vS[, id_combination := .I]
-
-            # Compute the new Shapley values
-            shap_names <- true$internal$parameters$feature_names
-            kshap <- t(this_W %*% as.matrix(processed_vS_list$dt_vS[, -"id_combination"]))
-            dt_kshap <- data.table::as.data.table(kshap)
-            colnames(dt_kshap) <- c("none", shap_names)
-            dt_shapley = dt_kshap
-
-            # Store the results
-            save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = list(shapley_values = dt_shapley,
-                                                                                          MSEv = NULL,
-                                                                                          only_save = list(X = this_X,
-                                                                                                           W = this_W,
-                                                                                                           S = S_sampled))
           }
-        }
 
-        # Save to disk
-        saveRDS(object = save_list, file = save_name)
+          # Save to disk
+          saveRDS(object = save_list, file = save_name)
+        }
       }
     }
   }
