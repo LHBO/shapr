48e1e7c9f2041725329e411c79208edd2bff4be7
LHBO <lholsen@math.uio.no>
2024-08-29 16:51:41 +0200

    WINE

--- R/Lars_explore_ideas_scripts/Simulation_Extra_Strategies_WINE.R
+++ R/Lars_explore_ideas_scripts/Simulation_Extra_Strategies_WINE.R
@@ -53,25 +53,63 @@ sum_shapley_weights <- function(m){
 
 
 # Code starts -----------------------------------------------------------------------------------------------------
+# Get the name of the computer we are working on
+hostname = R.utils::System$getHostname()
+cat(sprintf("We are working on '%s'.\n", R.utils::System$getHostname()))
+
+# Check if we are working on an UiO computer or not and define the correct folder based on system
+if (hostname == "Larss-MacBook-Pro.local" || Sys.info()[[7]] == "larsolsen") {
+  # Where the files are stored
+  folder = "/Users/larsolsen/PhD/Paper3/shapr"
+  folder_save = "/Users/larsolsen/PhD/Paper3/Paper3_save_location"
+  UiO = FALSE
+} else if (grepl("hpc.uio.no", hostname)) {
+  # TBA
+  folder = ""
+  UiO = TRUE
+} else if (grepl("uio.no", hostname)) {
+  folder = "/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/shapr"
+  folder_save = "/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location"
+  UiO = TRUE
+} else {
+  stop("We do not recongize the system at which the code is run (not Lars's MAC, HPC, nor UiO).")
+}
+
+
+
 repetitions = 100
 new_strategies = c("on_all_cond", "on_all_cond_paired")
+new_strategies = c("on_all_cond_paired")
 versions = c("largest_weights_random", "unique_paired")
-weight_versions = c("analytical", "non_analytical")
-resave = TRUE
+weight_versions = c("analytical", "non_analytical", "mean_L", "mean_ps")
 
+resave = FALSE
+
+args = commandArgs(trailingOnly = TRUE)
+version_cmd = as.character(args[1])
+if (!is.null(version_cmd)) versions = version_cmd
 
 
 message("Reading TRUE")
-true = readRDS(paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/Wine_data_sep_rf.rds"))
+true = readRDS(file.path(folder_save, "Wine_data_sep_rf.rds"))
 precomputed_vS = true$internal$output # Extract only the precomputed_vS list
+true_SV = as.matrix(true$shapley_values)
 message("DONE Reading TRUE")
+
+# Get the weights
+message("Reading dt weights")
+dt_new_weights_analytical = readRDS(file.path(folder_save, paste0("Analytical_prop_M_11_res.rds")))
+dt_new_weights_sequence = readRDS(file.path(folder_save, paste0("Sequence_length_M_11_combined.rds")))
+dt_new_weights_sequence = dcast(dt_new_weights_sequence[type == "mean"], M + N_S + Size ~ version, value.var = "Ps_tilde")
+message("DONE Reading dt weights")
+
 
 
 for (version in versions) {
   if (version == "unique_paired") {
-    file_name = "/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/NEW_Wine_data_res_unique_paired.rds"
+    file_name = file.path(folder_save, "NEW_Wine_data_res_unique_paired.rds")
   } else if (version == "largest_weights_random") {
-    file_name = "/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/NEW_Wine_data_res_largest_weights_random_new_weights_empirical.rds"
+    file_name = file.path(folder_save, "NEW_Wine_data_res_largest_weights_random_new_weights_empirical.rds")
   } else {
     stop("Unknown version")
   }
@@ -80,66 +118,55 @@ for (version in versions) {
   file = readRDS(file_name)
   message("DONE Reading FILE")
 
+  n_combinations_names = names(file$res)
+  n_combinations = as.integer(sub(".*_", "", n_combinations_names))
+  rep_names = names(file$res$n_combinations_2)
+  repetitions_seq = as.integer(sub(".*_", "", rep_names))
 
 
 
+  # Iterate over the strategies
+  strategy = new_strategies[1]
+  for (strategy in new_strategies) {
+    for (weight_version in weight_versions) {
+      strategy_name = paste(strategy, version, weight_version, sep = "_")
 
+      # Get the strategy specific save name
+      save_name = file.path(folder_save, paste0("NEW_Wine_data_res_", strategy_name, ".rds"))
 
+      # List to store the results for each new strategy
+      save_list = list(res = list())
 
+      # data table to store the MAE
+      res_dt = data.table(Strategy = character(), n_combinations = numeric(), repetition = integer(), MAE = numeric())
 
-}
-
-
-
-
-
-
-# Iterate over the repetitions
-repetition = 1
-for (repetition in seq(repetitions)) {
-  message("Reading FILE")
-  file_name = paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/M_17_n_train_1000_n_test_500_rho_", rho, "_equi_FALSE_betas_2_10_0.25_-3_-1_1.5_-0.5_10_1.25_1.5_-2_3_-1_-5_4_-10_2_5_estimated_repetition_", repetition ,".rds")
-  if (!file.exists(file_name)) next
-  file = readRDS(file_name)
-  message("DONE Reading FILE")
-
-  version = "unique_paired"
-  for (version in versions) {
-    # Get the relevant files
-    #file_relevant = file$unique_paired$repetition_1
-    file_relevant = file[[version]]$repetition_1
-    file_relevant_names = names(file_relevant)
-    n_combinations = as.integer(sub(".*_", "", names(file_relevant)))
-
-    # Iterate over the strategies
-    strategy = new_strategies[1]
-    for (strategy in new_strategies) {
-      for (weight_version in weight_versions) {
-        strategy_name = paste(strategy, version, weight_version, sep = "_")
-
-        # Get the strategy specific save name
-        save_name = paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/M_17_n_train_1000_n_test_500_rho_", rho, "_equi_FALSE_betas_2_10_0.25_-3_-1_1.5_-0.5_10_1.25_1.5_-2_3_-1_-5_4_-10_2_5_estimated_repetition_", repetition ,"_", strategy_name, ".rds")
-
-        # List to store the results for each new strategy
-        save_list = list()
-        save_list[[strategy_name]] = list(repetition_1 = list())
+      # Iterate over the repetitions
+      for (repetition in repetitions_seq) {
+        rep_now = rep_names[repetition]
 
         # Iterate over the different number of coalitions
-        n_comb_now_idx = 2
+        n_comb_now_idx = 20
         for (n_comb_now_idx in seq_along(n_combinations)) {
-          message(sprintf("version = %s \t weight_version = %s \t rho = %.1f \t repetition = %d \t strategy = %s \t n_comb = %d",
-                          version, weight_version, rho, repetition, strategy, n_combinations[n_comb_now_idx]))
+          n_comb_now = n_combinations_names[n_comb_now_idx]
+          if (n_comb_now_idx %% round(length(n_combinations) / 10) == 0) {
+            message(sprintf("version = %s \t weight_version = %s \t repetition = %d \t strategy = %s \t n_comb = %d",
+                            version, weight_version, repetition, strategy, n_combinations[n_comb_now_idx]))
+          }
 
           if (n_combinations[n_comb_now_idx] == 2) {
             # Only empty and grand coalition so just copy
-            save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = file_relevant$n_combinations_2
-            # save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]]$internal$parameters$precomputed_vS = NULL
+            save_list$res[[n_comb_now]][[rep_now]] = file$res$n_combinations_2[[rep_now]]
 
           } else {
             #
-            file_now = file_relevant[[n_comb_now_idx]]
 
-            this_X = data.table::copy(file_now$only_save$X)
+            file_now = file$res[[n_comb_now]][[rep_now]]
+
+
+            # Get the X data table
+            this_X = data.table::copy(file_now$internal$objects$X)
+            this_X[, shapley_weight := as.numeric(shapley_weight)]
+
 
             if (version == "largest_weights_random") { # Set weights to 1 as the coals are not sampled with replacement
               this_X[-c(1,.N), shapley_weight := 1.0]
@@ -149,7 +176,7 @@ for (repetition in seq(repetitions)) {
             shapley_reweighting(this_X, reweight = strategy)
 
             if (weight_version == "analytical") {
-              dt_new_weights = readRDS(paste0("/mn/kadingir/biginsight_000000/lholsen/PhD/Paper3/Paper3_save_location/Analytical_prop_M_", 17, "_res.rds"))
+              dt_new_weights = copy(dt_new_weights_analytical)
 
               # Find the weights of the combination closest to n_combinations
               n_comb_use = dt_new_weights$n_combinations[which.min(abs(dt_new_weights$n_combinations - n_combinations[n_comb_now_idx]))]
@@ -157,8 +184,25 @@ for (repetition in seq(repetitions)) {
 
               # Update the weights with the provided weights for each coalition size
               this_X[dt_new_weights_now, on = "n_features", shapley_weight := get("mean")]
-            }
+            } else if (weight_version %in% c("mean_L", "mean_ps")) {
 
+              # Get the weights
+              dt_new_weights = copy(dt_new_weights_sequence)
+
+              # Find the weights of the combination closest to n_combinations
+              n_comb_use = dt_new_weights$N_S[which.min(abs(dt_new_weights$N_S - n_combinations[n_comb_now_idx]))]
+              dt_new_weights_now = dt_new_weights[N_S == n_comb_use]
+
+              dt_new_weights_now <- rbind(dt_new_weights_now, dt_new_weights_now[(.N - ifelse(.N %% 2 == 1, 1, 0)):1])
+              dt_new_weights_now[, Size := seq(.N)]
+              setnames(dt_new_weights_now, "Size", "n_features")
+
+              # Update the weights with the provided weights for each coalition size
+              this_X[dt_new_weights_now, on = "n_features", shapley_weight := get(gsub("_", " ", weight_version))]
+
+            } else {
+              stop("Unknown weight_version")
+            }
 
             # Get the weight matrix
             this_W <- shapr:::weight_matrix(X = this_X, normalize_W_weights = TRUE)
@@ -195,18 +239,38 @@ for (repetition in seq(repetitions)) {
             dt_shapley = dt_kshap
 
             # Store the results
-            save_list[[strategy_name]][[1]][[file_relevant_names[n_comb_now_idx]]] = list(shapley_values = dt_shapley,
-                                                                                          MSEv = NULL,
-                                                                                          only_save = list(X = this_X,
-                                                                                                           W = this_W,
-                                                                                                           S = S_sampled))
-          }
-        }
+            save_list$res[[n_comb_now]][[rep_now]] = list(shapley_values = dt_shapley,
+                                                          internal = list(objects = list(X = this_X,
+                                                                                         W = this_W,
+                                                                                         S = S_sampled)),
+                                                          pred_explain = NULL,
+                                                          MSEv = NULL,
+                                                          timing = NULL)
 
-        # Save to disk
-        saveRDS(object = save_list, file = save_name)
-      }
-    }
-  }
-}
+            # Compute the MAE and add it to the data table
+            res_dt = rbind(
+              res_dt,
+              data.table(
+                Strategy = strategy_name,
+                n_combinations = n_combinations[n_comb_now_idx],
+                repetition = repetition,
+                MAE = compute_MAE_MSE_fast(as.matrix(dt_kshap), true_SV, "MAE")
+              )
+            )
+          }
+        } # End combinations
+      } # End repetitions
+
+      # Order the rows
+      setorderv(res_dt, c("n_combinations", "repetition"))
+      save_list$res_dt = res_dt
+
+      # Save to disk
+      saveRDS(object = save_list, file = save_name)
+      compute_MAE_MSE_fast(as.matrix(dt_kshap), true_SV, "MAE")
+    } # Weight_version
+  } # Strategy
+} # Version
+
+
 
